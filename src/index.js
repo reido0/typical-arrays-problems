9271d0f36e1b23631d19603c6ceb00d7075dc21d
Matveev Anton <reido@mail.ru>
2021-08-09 22:11:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
+  if (array == undefined || array.length === 0) {
+    return 0;
+  } else {
+    return Math.min(...array);
+  };
+};
 
-exports.min = function min (array) {
-  return 0;
-}
 
-exports.max = function max (array) {
-  return 0;
-}
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array == undefined || array.length === 0) {
+    return 0;
+  } else {
+    return Math.max(...array);
+  };
+};
+
+
+
+exports.avg = function avg(array) {
+  if (array == undefined || array.length === 0) {
+    return 0;
+  } else {
+    return array.reduce((a, b) => (a + b)) / array.length;
+  };
+};
